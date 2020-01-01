4576c7422f359e8ecc3643a554cf55fd59a25f38
jihu02@naver.com <jihu02@naver.com>
2020-01-02 00:19:28 +0900

    [CREATE] 세번째 information 화면 구성 & 앱아이콘 추가

--- app/src/main/java/com/example/gitcat/Info2Activity.kt
+++ app/src/main/java/com/example/gitcat/Info2Activity.kt
@@ -1,12 +1,21 @@
 package com.example.gitcat
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_info2.*
 
 class Info2Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info2)
+
+        buttonGo.setOnClickListener{
+
+            //화면 이동
+            var intent = Intent(this,Info3Activity::class.java)
+            startActivity(intent)
+        }
     }
 }

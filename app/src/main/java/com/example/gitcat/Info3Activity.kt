982b1dde16c62798a4e2f142c4e4d49e0e8f5994
jihu02@naver.com <jihu02@naver.com>
2020-01-03 23:55:53 +0900

    [CREATE] 정보화면 네번째 화면 생성

--- app/src/main/java/com/example/gitcat/Info3Activity.kt
+++ app/src/main/java/com/example/gitcat/Info3Activity.kt
@@ -1,12 +1,21 @@
 package com.example.gitcat
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_info3.*
 
 class Info3Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info3)
+
+        buttonGo.setOnClickListener{
+
+            //화면 이동
+            var intent = Intent(this,Info4Activity::class.java)
+            startActivity(intent)
+        }
     }
 }

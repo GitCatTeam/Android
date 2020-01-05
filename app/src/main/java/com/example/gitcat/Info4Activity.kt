98f2afee599641d9e6a74fefc49a1afc76609172
jihu02@naver.com <jihu02@naver.com>
2020-01-05 20:25:06 +0900

    [CREATE] 정보 화면 5가지 구성, 위치 배치 및 디자인 수정 필요

--- app/src/main/java/com/example/gitcat/Info4Activity.kt
+++ app/src/main/java/com/example/gitcat/Info4Activity.kt
@@ -1,12 +1,21 @@
 package com.example.gitcat
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_info4.*
 
 class Info4Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info4)
+
+        buttonGo.setOnClickListener{
+
+            //화면 이동
+            var intent = Intent(this,Info5Activity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/gitcat/Info5Activity.kt
@@ -0,0 +1,12 @@
+package com.example.gitcat
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Info5Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info5)
+    }
+}

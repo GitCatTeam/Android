db2af4bc94be6389373c4269648ed5f998e1f1a6
jihu02@naver.com <jihu02@naver.com>
2020-01-09 01:19:06 +0900

    [UPDATE] 홈화면 수정

--- app/src/main/java/com/example/gitcat/HomeActivity.kt
+++ app/src/main/java/com/example/gitcat/HomeActivity.kt
@@ -3,6 +3,8 @@ package com.example.gitcat
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
@@ -10,6 +12,10 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        //툴바 적용
+        setSupportActionBar(findViewById(R.id.toolbar))
+        getSupportActionBar()?.title = ""
 
         //초기화
         loadFragment(HomeFragment())
@@ -40,5 +46,25 @@ class HomeActivity : AppCompatActivity() {
         transaction.replace(R.id.container, fragment)
         transaction.addToBackStack(null)
         transaction.commit()
+    }
+
+    //액션버튼 메뉴 액션바에 집어 넣기
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.action_bar,menu)
+        return true
+    }
+
+    //액션버튼 클릭 했을 때
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId){
+            R.id.settings -> {
+                //설정 버튼을 눌렀을 때
+                return super.onOptionsItemSelected(item)
+            }
+            R.id.diary -> {
+                return super.onOptionsItemSelected(item)
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
     }
 }

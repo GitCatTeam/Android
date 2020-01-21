920641c62f15fafd469c472f96aee545de5f6979
jihu02@naver.com <jihu02@naver.com>
2020-01-21 22:06:12 +0900

    [CREATE] 냥이어리 추가

---
+++ app/src/main/java/com/example/gitcat/DiaryActivity.kt
@@ -0,0 +1,12 @@
+package com.example.gitcat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DiaryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_diary)
+    }
+}

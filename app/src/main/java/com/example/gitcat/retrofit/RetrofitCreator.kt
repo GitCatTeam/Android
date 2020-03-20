c95ac9146154f71dcbbdcc9a068d1006cd4a34d5
jihu02@naver.com <jihu02@naver.com>
2020-03-21 01:00:26 +0900

    [UPDATE] 레퍼지토리별 커밋내역 기능 완료

--- app/src/main/java/com/example/gitcat/retrofit/RetrofitCreator.kt
+++ app/src/main/java/com/example/gitcat/retrofit/RetrofitCreator.kt
@@ -1,10 +1,6 @@
 package com.example.gitcat.retrofit
 
-import com.example.gitcat.BuildConfig
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitCreator {

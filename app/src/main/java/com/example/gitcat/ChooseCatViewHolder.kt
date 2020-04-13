d408bba54b0f969bd1936410f9a8d113ba1c2123
yanghyesun <nbn0809@naver.com>
2020-04-13 19:38:19 +0900

    [UPDATE] 기타 수정

--- app/src/main/java/com/example/gitcat/ChooseCatViewHolder.kt
+++ app/src/main/java/com/example/gitcat/ChooseCatViewHolder.kt
@@ -2,7 +2,10 @@ package com.example.gitcat
 
 import android.util.Log
 import android.view.View
+import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.RelativeLayout
+import androidx.cardview.widget.CardView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -11,12 +14,13 @@ import com.example.gitcat.model.ChooseCatBasicModel
 import kotlinx.android.synthetic.main.choose_cat_item.view.*
 
 class ChooseCatViewHolder(view: View):RecyclerView.ViewHolder(view){
-    val constraint: ConstraintLayout = view.findViewById(R.id.cl_choose_cat)
+    val relative = view.findViewById(R.id.cl_choose_cat) as RelativeLayout
     val img:ImageView = view.findViewById(R.id.img_choose_cat_item)
     fun bind(data: ChooseCatBasicModel){
         Glide.with(itemView).load(data.profileImg).into(img)
-        constraint.setOnClickListener {
-//            it.isSelected = !it.isSelected
+        relative.elevation = 8f
+        relative.setOnClickListener {
+            it.isSelected = !it.isSelected
         }
     }
     fun select(data: List<ChooseCatBasicModel>){

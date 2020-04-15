78c02e015646cb1fc9cebadfbed45d4216b1f572
yanghyesun <nbn0809@naver.com>
2020-04-16 02:28:17 +0900

    [UPDATE] 고양이 하나만 선택하기 완료

--- app/src/main/java/com/example/gitcat/ChooseCatRecyclerAdapter.kt
+++ app/src/main/java/com/example/gitcat/ChooseCatRecyclerAdapter.kt
@@ -1,6 +1,8 @@
 package com.example.gitcat
 
 import android.content.Context
+import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -8,6 +10,8 @@ import com.example.gitcat.model.ChooseCatBasicModel
 
 class ChooseCatRecyclerAdapter(private val context:Context):RecyclerView.Adapter<ChooseCatViewHolder>() {
     var data = listOf<ChooseCatBasicModel>()
+    var select = -1
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChooseCatViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.choose_cat_item,parent,false)
         return ChooseCatViewHolder(view)
@@ -17,6 +21,19 @@ class ChooseCatRecyclerAdapter(private val context:Context):RecyclerView.Adapter
 
     override fun onBindViewHolder(holder: ChooseCatViewHolder, position: Int) {
         holder.bind(data[position])
-        holder.select(data)
+        //하나만 선택
+        if(select == -1){
+            holder.relative.isSelected = false
+        }else{
+            holder.relative.isSelected = select == position
+        }
+        holder.relative.setOnClickListener{
+            holder.relative.isSelected = true
+            if(select != position) {
+                notifyItemChanged(select)
+                select = position
+            }
+        }
+
     }
 }
--- app/src/main/java/com/example/gitcat/ChooseCatViewHolder.kt
+++ app/src/main/java/com/example/gitcat/ChooseCatViewHolder.kt
@@ -1,30 +1,28 @@
 package com.example.gitcat
 
-import android.util.Log
 import android.view.View
-import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.RelativeLayout
-import androidx.cardview.widget.CardView
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.example.gitcat.R
 import com.example.gitcat.model.ChooseCatBasicModel
-import kotlinx.android.synthetic.main.choose_cat_item.view.*
 
 class ChooseCatViewHolder(view: View):RecyclerView.ViewHolder(view){
+    var select = -1
     val relative = view.findViewById(R.id.cl_choose_cat) as RelativeLayout
     val img:ImageView = view.findViewById(R.id.img_choose_cat_item)
     fun bind(data: ChooseCatBasicModel){
         Glide.with(itemView).load(data.profileImg).into(img)
-        relative.elevation = 8f
-        relative.setOnClickListener {
-            it.isSelected = !it.isSelected
-        }
+//        if(select == -1){
+//            relative.isSelected = false
+//        }else{
+//            relative.isSelected = select == adapterPosition
+//        }
+//        relative.setOnClickListener{
+//            relative.isSelected = true
+//            if(select != adapterPosition) {
+//
+//            }
+//        }
     }
-    fun select(data: List<ChooseCatBasicModel>){
-
-    }
-
 }

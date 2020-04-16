91f3453775e5700ebc20f6782ea29048a2a1cbc4
yanghyesun <nbn0809@naver.com>
2020-04-16 22:23:01 +0900

    [CREATE] 준비중, 숨겨진 고양이 dialog 생성

---
+++ app/src/main/java/com/example/gitcat/PrepareDialogFragment.kt
@@ -0,0 +1,44 @@
+package com.example.gitcat
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_prepare_dialog.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class PrepareDialogFragment : DialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        val view = inflater.inflate(R.layout.fragment_prepare_dialog, container, false)
+
+        dialog?.window?.requestFeature(Window.FEATURE_NO_TITLE)
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog?.setCancelable(false)
+        dialog?.window?.setGravity(Gravity.CENTER)
+
+        return view
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        init()
+    }
+
+    fun init(){
+        img_btn_prepare_close.setOnClickListener {
+            dismiss()
+        }
+    }
+
+}

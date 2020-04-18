56d5ea94784e45be72199a92a8043739851ff904
jihu02@naver.com <jihu02@naver.com>
2020-04-19 00:03:04 +0900

    [UPDATE] 커밋달력, 리포트, 차트 UI 및 API 기능 마무리

--- app/src/main/java/com/example/gitcat/CalendarUnselectedDecorator.kt
+++ app/src/main/java/com/example/gitcat/CalendarUnselectedDecorator.kt
@@ -6,13 +6,13 @@ import com.prolificinteractive.materialcalendarview.CalendarDay
 import com.prolificinteractive.materialcalendarview.DayViewDecorator
 import com.prolificinteractive.materialcalendarview.DayViewFacade
 
-class CalendarSelectedDecorator : DayViewDecorator {
+class CalendarUnselectedDecorator : DayViewDecorator{
     private lateinit var drawable: Drawable
     private var dates: CalendarDay? = null
 
     constructor(dates: CalendarDay, context: Activity){
         this.dates = dates
-        drawable = context.getDrawable(R.drawable.calendar_select)
+        drawable = context.getDrawable(R.drawable.calendar_unselect)
     }
 
     override fun shouldDecorate(day: CalendarDay?): Boolean {
@@ -20,12 +20,6 @@ class CalendarSelectedDecorator : DayViewDecorator {
     }
 
     override fun decorate(view: DayViewFacade?) {
-        //view!!.setSelectionDrawable(drawable)
         view!!.setBackgroundDrawable(drawable)
-        //view!!.addSpan(DotSpan(5f, color))
     }
-
-
-
-
 }

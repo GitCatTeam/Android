1abd519eadabcb38c2b48abc7dbbf734bb851eb3
jihu02@naver.com <jihu02@naver.com>
2020-05-31 00:40:09 +0900

    [UPDATE] 400대 에러 팝업창 글귀 수정

--- app/src/main/java/com/example/gitcat/HomeFragment.kt
+++ app/src/main/java/com/example/gitcat/HomeFragment.kt
@@ -158,7 +158,7 @@ class HomeFragment : Fragment() {
                         if(response.code()>=500){
                             showErrorPopup("[네트워크 오류] 재로그인을 해주세요!",context!!)
                         }else{
-                            showErrorPopup("["+response.code().toString()+" 오류] "+"재로그인을 해주세요!",context!!)
+                            showErrorPopup("["+response.code().toString()+" 오류] "+"내부 서버 오류",context!!)
                         }
                     }
                 }
--- app/src/main/java/com/example/gitcat/SettingsFragment.kt
+++ app/src/main/java/com/example/gitcat/SettingsFragment.kt
@@ -76,7 +76,7 @@ class SettingsFragment : PreferenceFragmentCompat(){
                                 if(response.code()>=500){
                                     showErrorPopup("[네트워크 오류] 재로그인을 해주세요!",context!!)
                                 }else{
-                                    showErrorPopup("["+response.code().toString()+" 오류] "+"재로그인을 해주세요!",context!!)
+                                    showErrorPopup("["+response.code().toString()+" 오류] "+"내부 서버 오류",context!!)
                                 }
                             }
                         }

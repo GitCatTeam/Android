1b6435ea6e579f72ba08b7ad1c1e1fbd81eab265
jihu02@naver.com <jihu02@naver.com>
2020-09-27 22:05:42 +0900

    [UPDATE] 설정화면 피드백 보내기 추가
    - Intent로 email send 방식 추가
    - 수신자, 내용 양식 설정

--- app/src/main/java/com/catlove/gitcat/SettingsFragment.kt
+++ app/src/main/java/com/catlove/gitcat/SettingsFragment.kt
@@ -20,19 +20,21 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-
-class SettingsFragment : PreferenceFragmentCompat(){
+class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         //addPreferencesFromResource(R.xml.preferences)
-        setPreferencesFromResource(R.xml.preferences,rootKey)
+        setPreferencesFromResource(R.xml.preferences, rootKey)
 
         val set_cat = findPreference("set_cat") as Preference
         val set_repo = findPreference("set_repo") as Preference
-        val dialogView = layoutInflater.inflate(R.layout.settings_dialog,container,false)
+        val set_feedback = findPreference("etc_feedback") as Preference
+        val dialogView = layoutInflater.inflate(R.layout.settings_dialog, container, false)
 
-        val settings: SharedPreferences = requireActivity().getSharedPreferences("gitcat",
-            AppCompatActivity.MODE_PRIVATE)
+        val settings: SharedPreferences = requireActivity().getSharedPreferences(
+            "gitcat",
+            AppCompatActivity.MODE_PRIVATE
+        )
         val editor: SharedPreferences.Editor = settings.edit()
 
         val dialogTitle = dialogView.findViewById<TextView>(R.id.dialog_title)
@@ -50,7 +52,11 @@ class SettingsFragment : PreferenceFragmentCompat(){
             ad.setView(dialogView)
 
             dialogTitle.text = getString(R.string.settings_popup_repo1)
-            dialogMessage.text = getString(R.string.settings_popup_repo2) + " " + settings.getString("repoAuth","") + " " + getString(R.string.settings_popup_repo3)
+            dialogMessage.text =
+                getString(R.string.settings_popup_repo2) + " " + settings.getString(
+                    "repoAuth",
+                    ""
+                ) + " " + getString(R.string.settings_popup_repo3)
 
             dialogCancel.setOnClickListener {
                 ad.dismiss()
@@ -60,68 +66,70 @@ class SettingsFragment : PreferenceFragmentCompat(){
                 NewToken(context!!)
 
                 //디바이스토큰 삭제
-                val ai = DeviceIdModel(settings.getString("androidId","")!!)
-                val call2: Call<DeviceIdModel> = RetrofitCreator.service.deleteDeviceToken(settings.getString("token","")!!,ai)
+                val ai = DeviceIdModel(settings.getString("androidId", "")!!)
+                val call2: Call<DeviceIdModel> =
+                    RetrofitCreator.service.deleteDeviceToken(settings.getString("token", "")!!, ai)
                 call2.enqueue(
                     object : Callback<DeviceIdModel> {
                         override fun onFailure(call: Call<DeviceIdModel>, t: Throwable) {
                             Log.e("*+*+", "error: $t")
-                            showErrorPopup("재로그인을 해주세요!",context!!)
+                            showErrorPopup("재로그인을 해주세요!", context!!)
                         }
 
                         override fun onResponse(
                             call: Call<DeviceIdModel>,
                             response: Response<DeviceIdModel>
                         ) {
-                            if(response.isSuccessful){
-                                d("*+*+디바이스토큰","디바이스 토큰 삭제 성공")
+                            if (response.isSuccessful) {
+                                d("*+*+디바이스토큰", "디바이스 토큰 삭제 성공")
 
-                            }else{
-                                if(response.code()==503){
+                            } else {
+                                if (response.code() == 503) {
                                     ServerCheckPopup(context!!)
-                                }else if(response.code()>=500){
-                                    showErrorPopup("[네트워크 오류] 재로그인을 해주세요!",context!!)
-                                }else{
-                                    showErrorPopup("재로그인을 해주세요!",context!!)
+                                } else if (response.code() >= 500) {
+                                    showErrorPopup("[네트워크 오류] 재로그인을 해주세요!", context!!)
+                                } else {
+                                    showErrorPopup("재로그인을 해주세요!", context!!)
                                 }
                             }
                         }
                     }
                 )
 
-                val call: Call<LogoutModel> = RetrofitCreator.service.postLogout(settings.getString("token","")!!)
+                val call: Call<LogoutModel> =
+                    RetrofitCreator.service.postLogout(settings.getString("token", "")!!)
                 call.enqueue(
                     object : Callback<LogoutModel> {
                         override fun onFailure(call: Call<LogoutModel>, t: Throwable) {
                             Log.e("*+*+", "error: $t")
-                            showErrorPopup("재로그인을 해주세요!",context!!)
+                            showErrorPopup("재로그인을 해주세요!", context!!)
                         }
 
                         override fun onResponse(
                             call: Call<LogoutModel>,
                             response: Response<LogoutModel>
                         ) {
-                            if(response.isSuccessful){
+                            if (response.isSuccessful) {
                                 //로그아웃
                                 settings.edit().clear().commit()
 
                                 //레포계정
-                                editor.putBoolean("newPeople",true)
+                                editor.putBoolean("newPeople", true)
                                 editor.commit()
 
-                                var intent = Intent(context!!,MainActivity::class.java)
+                                var intent = Intent(context!!, MainActivity::class.java)
                                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
                                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
                                 startActivity(intent)
 
 
-                            }else{
-                                if(response.code()==503){
+                            } else {
+                                if (response.code() == 503) {
                                     ServerCheckPopup(context!!)
-                                }else if(response.code()>=500){
-                                    showErrorPopup("[네트워크 오류] 재로그인을 해주세요!",context!!)
-                                }else{
-                                    showErrorPopup("재로그인을 해주세요!",context!!)
+                                } else if (response.code() >= 500) {
+                                    showErrorPopup("[네트워크 오류] 재로그인을 해주세요!", context!!)
+                                } else {
+                                    showErrorPopup("재로그인을 해주세요!", context!!)
                                 }
                             }
                         }
@@ -152,31 +160,32 @@ class SettingsFragment : PreferenceFragmentCompat(){
             }
             dialogOK.setOnClickListener {
                 NewToken(context!!)
-                val call: Call<Unit> = RetrofitCreator.service.deleteCats(settings.getString("token","")!!)
+                val call: Call<Unit> =
+                    RetrofitCreator.service.deleteCats(settings.getString("token", "")!!)
                 call.enqueue(
                     object : Callback<Unit> {
                         override fun onFailure(call: Call<Unit>, t: Throwable) {
                             Log.e("*+*+", "error: $t")
-                            showErrorPopup("재로그인을 해주세요!",context!!)
+                            showErrorPopup("재로그인을 해주세요!", context!!)
                         }
 
                         override fun onResponse(
                             call: Call<Unit>,
                             response: Response<Unit>
                         ) {
-                            if(response.isSuccessful){
+                            if (response.isSuccessful) {
                                 //고양이 초기화 API
-                                var intent = Intent(context!!,Info4Activity::class.java)
+                                var intent = Intent(context!!, Info4Activity::class.java)
                                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
                                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
                                 startActivity(intent)
-                            }else{
-                                if(response.code()==503){
+                            } else {
+                                if (response.code() == 503) {
                                     ServerCheckPopup(context!!)
-                                }else if(response.code()>=500){
-                                    showErrorPopup("[네트워크 오류] 재로그인을 해주세요!",context!!)
-                                }else{
-                                    showErrorPopup("재로그인을 해주세요!",context!!)
+                                } else if (response.code() >= 500) {
+                                    showErrorPopup("[네트워크 오류] 재로그인을 해주세요!", context!!)
+                                } else {
+                                    showErrorPopup("재로그인을 해주세요!", context!!)
                                 }
                             }
                         }
@@ -185,6 +194,28 @@ class SettingsFragment : PreferenceFragmentCompat(){
             }
 
             ad.show()
+
+            true
+        }
+
+        //피드백 보내기
+        val appVersion = BuildConfig.VERSION_NAME
+
+        set_feedback.setOnPreferenceClickListener {
+            val email = Intent(Intent.ACTION_SEND)
+            email.type = "plain/Text"
+            val gitcatemail: Array<String> = arrayOf("gitcatlove@gmail.com")
+            email.putExtra(Intent.EXTRA_EMAIL, gitcatemail)
+//            email.putExtra(
+//                Intent.EXTRA_SUBJECT,
+//                "제목"
+//            )
+            email.putExtra(
+                Intent.EXTRA_TEXT,
+                "앱 버전 (AppVersion):$appVersion\n기기명 (Device):\n안드로이드 OS (Android OS):\n내용 (Content):\n"
+            )
+            email.type = "message/rfc822"
+            startActivity(email)
 
             true
         }
